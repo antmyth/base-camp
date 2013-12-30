1cb8065e42e42cb589c3940ccca840359d59d7e3
Antonio Pedro Nascimento <antonio.p.nascimento@gmail.com>
2013-12-30 14:31:26 +0000

    implemented someString

--- data-fixtures/src/main/java/basecamp/datafixtures/PrimitiveDataFixtures.java
+++ data-fixtures/src/main/java/basecamp/datafixtures/PrimitiveDataFixtures.java
@@ -1,8 +1,20 @@
 package basecamp.datafixtures;
 
+import java.util.Random;
+
 public class PrimitiveDataFixtures {
 
-    public static String someString(){
-        return "ABCDEFGH"+2;
-    }
+	private static Random random = new Random();
+
+	public static String someString(){
+        StringBuilder sb = new StringBuilder(8);
+		for (int i = 0; i < 8; i++) {
+			sb.append(String.valueOf((char)(random.nextInt(26)+'A')));
+		}
+		return sb.toString();
+	}
+
+	public static String someStringOfLength(int length){
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
 }
