789dfb2d7d44e065679f29da496aaa3817907176
antmyth <antonio.p.nascimento@gmail.com>
2013-12-30 15:03:14 +0000

    started work on the data fixtures 2

--- data-fixtures/src/test/java/basecamp/datafixtures/PrimitiveDataFixturesTest.java
+++ data-fixtures/src/test/java/basecamp/datafixtures/PrimitiveDataFixturesTest.java
@@ -1,23 +1,30 @@
 package basecamp.datafixtures;
 
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static basecamp.datafixtures.PrimitiveDataFixtures.someString;
+import static basecamp.datafixtures.PrimitiveDataFixtures.someStringOfLength;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.number.OrderingComparison.greaterThanOrEqualTo;
+import static org.hamcrest.number.OrderingComparison.lessThanOrEqualTo;
 import static org.junit.Assert.assertThat;
 
 public class PrimitiveDataFixturesTest {
 
     @Test
-    public void someStringReturnsAStringOfLength8() throws Exception {
-        assertThat(someString().length(),is(8));
+    public void someStringReturnsAStringWithLenghtBetween8And12() throws Exception {
+        assertThat(someString().length(),is(greaterThanOrEqualTo(8)));
+        assertThat(someString().length(),is(lessThanOrEqualTo(12)));
     }
 
     @Test
     public void someStringShouldReturnJustUpperCaseLetters() throws Exception {
-        assertThat(StringUtils.isAlpha(someString()),is(true));
+		String someString = someString();
+		assertThat(StringUtils.isAlpha(someString),is(true));
+        assertThat(StringUtils.isAllUpperCase(someString),is(true));
     }
 
     @Test
@@ -29,4 +36,11 @@ public class PrimitiveDataFixturesTest {
             first = second;
         }
     }
+	
+	@Ignore
+	@Test
+	public void someStringOfLengthShouldAlwaysReturnTheRequestedSizeString() throws Exception{
+		int length = 15;
+		assertThat(someStringOfLength(length).length(),is(length));
+	}
 }
